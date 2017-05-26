c630b5b16164f5c447d34aefe907eaa8698623a9
imranfarid <imran.cyborg@gmail.com>
2017-05-26 21:37:46 +0600

    directive examples

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="test">
-        <h1>Test</h1>
+        <!-- <span v-html="title"> </span> -->
+        <h1>{{title}}</h1>
+        <p>{{user.first_name}}</p>
+        <p v-text="user.first_name"></p>
     </div>
 </template>
 
 <script>
     export default{
-        name: 'test'
+        name: 'test',
+        data(){
+            return{
+                title: 'Hello World',
+                user: {
+                    first_name: 'Imran',
+                    last_name: 'Farid'
+                }
+            }
+        }
     }
 </script>
 
