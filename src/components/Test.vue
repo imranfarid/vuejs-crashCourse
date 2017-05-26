8e2f94ee4838d353e5e4d6471de76e59816e9378
imranfarid <imran.cyborg@gmail.com>
2017-05-26 22:22:14 +0600

    for-loop & events

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="test">
+        <input type="text" v-model="title"><br/>
         <!-- <span v-html="title"> </span> -->
         <h1>{{title}}</h1>
         <p v-if="showName">{{user.first_name}}</p>
         <p v-else>{{user.last_name}}</p>
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+        <button v-on:click="greet('Hello World')">Say greeting</button><br />
+        <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
     </div>
 </template>
 
@@ -17,7 +23,23 @@
                     first_name: 'Imran',
                     last_name: 'Farid'
                 },
-                showName: true
+                showName: true,
+                items: [
+                    {title: 'Item One'},
+                    {title: 'Item Two'},
+                    {title: 'Item Three'},
+                ]
+            }
+        },
+        methods: {
+            greet: function(greeting){
+                alert(greeting);
+            },
+            pressKey: function(e){
+                console.log(e.target.value);
+            },
+            enterHit: function(){
+                console.log('You hit enter')
             }
         }
     }
